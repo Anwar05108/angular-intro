b25b1ef8ef1c071438479fa2b14ffac73df5dec0
unknown <abdullahanwar133@gmail.com>
2022-04-26 05:26:46 +0600

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,77 @@
+import { Hero } from './hero';
+
+// export const HEROES: Hero[] = [
+//   { id: 11, name: 'Dr Nice' },
+//   { id: 12, name: 'Narco' },
+//   { id: 13, name: 'Bombasto' },
+//   { id: 14, name: 'Celeritas' },
+//   { id: 15, name: 'Magneta' },
+//   { id: 16, name: 'RubberMan' },
+//   { id: 17, name: 'Dynama' },
+//   { id: 18, name: 'Dr IQ' },
+//   { id: 19, name: 'Magma' },
+//   { id: 20, name: 'Tornado' },
+// ];
+
+export const HEROES: Hero[] = [
+  {
+    id: 1,
+    name: 'Windstorm',
+    power: 'Weather manipulation',
+    alterEgo: "Dr. Henry 'Hank' McCoy",
+  },
+  {
+    id: 2,
+    name: 'Superman',
+    power: 'Superhuman strength',
+    alterEgo: 'Clark Kent',
+  },
+  {
+    id: 3,
+    name: 'Batman',
+    power: 'Money',
+    alterEgo: 'Bruce Wayne',
+  },
+  {
+    id: 4,
+    name: 'Spiderman',
+    power: 'Web-slinging',
+    alterEgo: 'Peter Parker',
+  },
+  {
+    id: 5,
+    name: 'Ironman',
+    power: 'Tech',
+    alterEgo: 'Tony Stark',
+  },
+  {
+    id: 6,
+    name: 'Antman',
+    power: 'Ants',
+    alterEgo: 'Scott Lang',
+  },
+  {
+    id: 7,
+    name: 'Hulk',
+    power: 'Superhuman strength',
+    alterEgo: 'Bruce Banner',
+  },
+  {
+    id: 8,
+    name: 'Thor',
+    power: 'Thunder powers',
+    alterEgo: 'Thor Odinson',
+  },
+  {
+    id: 9,
+    name: 'Captain America',
+    power: 'Superhuman strength',
+    alterEgo: 'Steve Rogers',
+  },
+  {
+    id: 10,
+    name: 'Black Widow',
+    power: 'Superhuman agility',
+    alterEgo: 'Natasha Romanoff',
+  },
+];
